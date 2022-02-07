a48a6ba2eaf21d0ee7c600ee19b0e578e3ddeb19
Kamal <kamal@kuberarchitect24.builders>
2022-02-07 23:25:07 +0400

    fix remote state

--- main.tf
+++ main.tf
@@ -28,6 +28,11 @@ terraform {
   }
 }
 
+resource "null_resource" "example" {
+  triggers = {
+    value = "A example resource that does nothing!"
+  }
+}
 
 provider "aws" {
   region = var.aws_region
